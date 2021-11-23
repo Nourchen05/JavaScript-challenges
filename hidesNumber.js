4a584cc29b4e567f74cf3ae9a91992471f71fec5
Nourchen05 <noamennourchen5@gmail.com>
2021-11-23 22:00:08 +0100

    second exercice

---
+++ hidesNumber.js
@@ -0,0 +1,8 @@
+//javascript function that hides credit card numbers in a string.
+
+const hideCard=(str)=>{
+   return str.replace(/\b(?:\d{4}[ -]?){3}(?=\d{4}\b)/gm,'#### #### ####');
+}
+
+console.lo(hideCard('visa 1234 3846 3947 0971'));
+//visa #### #### #### 0971
